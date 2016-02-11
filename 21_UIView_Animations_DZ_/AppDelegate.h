75a9c0307b097a65da2ad503bfde148a8f0a0293
Stanly Shiyanovskiy <st-small@rambler.ru>
2016-02-11 11:22:23 +0200

    Initial Commit

---
+++ 21_UIView_Animations_DZ_/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  21_UIView_Animations_DZ_
+//
+//  Created by Stas on 11.02.16.
+//  Copyright © 2016 Stas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 21_UIView_Animations_DZ_/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  21_UIView_Animations_DZ_
+//
+//  Created by Stas on 11.02.16.
+//  Copyright © 2016 Stas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
